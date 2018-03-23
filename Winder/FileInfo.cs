9e0e564e7d520c56f460ace5c77ea90b27c76baf
Arseniy Banayev <arseniy.banayev@gmail.com>
2018-03-23 00:59:27 -0400

    Use font from Finder and wrap the FileSystemInfo classes as models

---
+++ Winder/FileInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Winder
+{
+	public abstract class FileSystemInfoExtended
+	{
+		protected FileSystemInfo SourceUntyped { get; }
+
+		protected FileSystemInfoExtended(FileSystemInfo source)
+		{
+			SourceUntyped = source;
+		}
+
+		public static FileSystemInfoExtended Create(FileSystemInfo source)
+		{
+			if (source is DirectoryInfo directoryInfo)
+				return new DirectoryInfoExtended(directoryInfo);
+			if (source is FileInfo fileInfo)
+				return new FileInfoExtended(fileInfo);
+			throw new NotSupportedException($"{nameof(FileSystemInfoExtended)} doesn't support {source.GetType().Name}");
+		}
+
+		public string Name => SourceUntyped.Name;
+
+		public abstract bool IsDirectory { get; }
+	}
+
+	public class DirectoryInfoExtended : FileSystemInfoExtended
+	{
+		public DirectoryInfoExtended(DirectoryInfo source) : base(source) { }
+
+		public DirectoryInfo Source => (DirectoryInfo)SourceUntyped;
+
+		public override bool IsDirectory => true;
+	}
+
+	public class FileInfoExtended : FileSystemInfoExtended
+	{
+		public FileInfoExtended(FileInfo source) : base(source) { }
+
+		public FileInfo Source => (FileInfo)SourceUntyped;
+
+		public override bool IsDirectory => false;
+	}
+}
--- Winder/MainWindow.xaml.cs
+++ Winder/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Winder
 {
@@ -22,11 +13,32 @@ namespace Winder
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private DirectoryInfo _currentDirectory;
+
 		public MainWindow()
 		{
-			InitializeComponent();
+			InitializeComponent(); // Always needs to happen first
+
+			// Triggers the font family and size to update to what is defined in the xaml window style
+			StyleProperty.OverrideMetadata(typeof(Window), new FrameworkPropertyMetadata {
+				DefaultValue = FindResource(typeof(Window))
+			});
+
+			// TODO: Make a model? Controller?
+			_currentDirectory = new DirectoryInfo(@"C:\Users\arsen\Sheet Music");
+			ListBoxFiles.ItemsSource = _currentDirectory.GetFileSystemInfos()
+				.Select(FileSystemInfoExtended.Create);
+
+			// Called when anything changes about the selection
+			ListBoxFiles.SelectionChanged += ListBoxFiles_SelectionChanged;
 		}
 
+		private void ListBoxFiles_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			Console.WriteLine("Selected changed");
+		}
+
+		#region Disable the default Window buttons (close, minimize)
+		
 		private const int GWL_STYLE = -16;
 		private const int WS_SYSMENU = 0x80000;
 
@@ -36,10 +48,12 @@ namespace Winder
 		[DllImport("user32.dll")]
 		private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-		private void Window_Loaded(object sender, RoutedEventArgs e)
-		{
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
 			var hwnd = new WindowInteropHelper(this).Handle;
 			SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
 		}
+
+		#endregion
+
 	}
 }
