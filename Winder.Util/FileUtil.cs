3a4852e50faa35c67816fa73cd4fbc21cc04796e
Arseniy Banayev <arseniy.banayev@gmail.com>
2018-04-02 17:18:34 -0400

    Fix build

---
+++ Winder.Util/FileUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Winder.Util
+{
+	public static class FileUtil
+	{
+		/// <summary>
+		/// If the extension of `file` is ".lnk" then returns the target of the shortcut.
+		/// Otherwise, returns `file`.
+		/// </summary>
+		public static string GetShortcutTarget(string file) {
+			if (Path.GetExtension(file)?.ToLower().Equals(".lnk", StringComparison.OrdinalIgnoreCase) != true)
+				return file;
+
+			var shellObjectType = Type.GetTypeFromProgID("WScript.Shell");
+			dynamic windowsShell = Activator.CreateInstance(shellObjectType);
+			var shortcut = windowsShell.CreateShortcut(file);
+			var targetPath = shortcut.TargetPath;
+			
+			// Release the COM objects
+			shortcut = null;
+			windowsShell = null;
+			return targetPath;
+		}
+	}
+}
